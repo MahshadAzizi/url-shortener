7fffcab81e2775f1cc8d6a6baa4a0518a9c4e5e0
MahshadAzizi <mahshad.azizi98@gmail.com>
2026-08-27 19:53:09 +0330

    feat: add visit service

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -3,13 +3,17 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.infrastructure.database.session import get_db_session
+from app.infrastructure.database.session import (
+    get_db_session,
+    session_factory,
+)
 from app.repositories.url_repository import URLRepository
 from app.repositories.visit_repository import VisitRepository
 from app.services.url_service import URLService
+from app.services.visit_service import VisitService
 
 
-async def get_url_service(
+def get_url_service(
         session: Annotated[
             AsyncSession,
             Depends(get_db_session),
@@ -19,4 +23,10 @@ async def get_url_service(
         session=session,
         url_repository=URLRepository(session),
         visit_repository=VisitRepository(session),
+    )
+
+
+def get_visit_service() -> VisitService:
+    return VisitService(
+        session_factory=session_factory,
     )
---
+++ app/services/visit_service.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+)
+
+from app.models.visit import Visit
+from app.repositories.visit_repository import VisitRepository
+
+
+class VisitService:
+    def __init__(
+            self,
+            session_factory: async_sessionmaker[AsyncSession],
+    ) -> None:
+        self._session_factory = session_factory
+
+    async def record_visit(
+            self,
+            url_id: int,
+            ip_address: str | None,
+    ) -> None:
+        async with self._session_factory() as session:
+            repository = VisitRepository(session)
+
+            visit = Visit(
+                url_id=url_id,
+                ip_address=ip_address,
+            )
+
+            await repository.create(visit)
+
+            await session.commit()
