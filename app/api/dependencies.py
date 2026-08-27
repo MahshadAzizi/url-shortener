25526ebd6cbd02f93b5bd7bb00d1cb10f1757c86
MahshadAzizi <mahshad.azizi98@gmail.com>
2026-08-27 18:44:26 +0330

    feat: add dependencies

---
+++ app/api/dependencies.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.infrastructure.database.session import get_db_session
+from app.repositories.url_repository import URLRepository
+from app.repositories.visit_repository import VisitRepository
+from app.services.url_service import URLService
+
+
+async def get_url_service(
+        session: Annotated[
+            AsyncSession,
+            Depends(get_db_session),
+        ],
+) -> URLService:
+    return URLService(
+        session=session,
+        url_repository=URLRepository(session),
+        visit_repository=VisitRepository(session),
+    )
